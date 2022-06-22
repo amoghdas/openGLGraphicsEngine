aaded0eb0ae7881994bd1424980e7d4f9586ee7c
amoghdas <amoghdas2002@gmail.com>
2022-06-21 20:14:34 -0400

    rotation

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
 #include <string>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffSet = 0.0f;
 float triMaxOffSet = 0.7f;
-float triIncrement = 0.009f;
+float triIncrement = 0.01f;
 
+float curAngle = 0.0f;
 
 
 // Vertex Shader
@@ -21,10 +26,10 @@ static const char *vShader = "											\n\
 																		\n\
 layout (location = 0) in vec3 pos;										\n\
 																		\n\
-uniform float xMove;													\n\
+uniform mat4 model;														\n\
 																		\n\
 void main() {															\n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);	\n\
+	gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
 }";
 
 // Fragment Shader
@@ -114,7 +119,7 @@ void compileShaders() {
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 
 }
 
@@ -181,7 +186,10 @@ int main() {
 			direction = !direction;
 		}
 
-
+		curAngle += 0.5f;
+		if (curAngle >= 360) {
+			curAngle -= 360;
+		}
 
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -189,7 +197,11 @@ int main() {
 
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffSet);
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffSet, 0.0f, 0.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
